79eabb000a324a34959bcb89894fee6c9beb1ea1
rcy0228 <104890759+rcy0228@users.noreply.github.com>
2024-07-07 21:09:42 -0400

    Add files via upload

---
+++ modules/rgroup-n01655070/main.tf
@@ -0,0 +1,7 @@
+resource "azurerm_resource_group" "rg_name" {
+  name     = var.rg_name
+  location = var.location
+  tags 	   = var.common_tags
+}
+
+
---
+++ modules/rgroup-n01655070/variables.tf
@@ -0,0 +1,9 @@
+variable "rg_name" {
+  default = "n01655070-RG"
+}
+
+variable "location" {
+  default = "Canada Central"
+}
+
+variable "common_tags" {}
